aabdc7458e3f97d465b693bc4ced4a80a6f10f92
Alberto Pedrero Esteban <apedreroes@upsa.es>
2021-03-02 18:34:51 +0100

    Commit 1 - Aplicación funcionando con un servicio para tareas

---
+++ src/app/componentes/listado-tareas/listado-tareas.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { TareasService } from 'src/app/servicios/tareas.service';
+
+@Component({
+  selector: 'app-listado-tareas',
+  templateUrl: './listado-tareas.component.html',
+  styleUrls: ['./listado-tareas.component.css']
+})
+export class ListadoTareasComponent implements OnInit {
+
+   listaTareas;
+
+
+  constructor(private tareasService: TareasService) { }
+
+  ngOnInit(): void {
+    this.listaTareas = this.tareasService.obtenerTareas();
+  }
+
+}
